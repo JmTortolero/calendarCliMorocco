1779ff3adfa2a0433725403814707804f122c1d1
jose <jose.tortolero@atos.net>
2025-09-22 00:14:10 +0200

    update archivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MonitorComponent } from './components/monitor/monitor.component';
+import { CalendarUploadComponent } from './components/calendar/calendar-upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CalendarUploadComponent },
+  { path: 'monitor', component: MonitorComponent }
+];
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calendarCliMorocco');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calendarMorocco-cli');
   });
 });
